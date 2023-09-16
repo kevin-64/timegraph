aa1484de807512102240ae1a0a75e58ae1e6b0ff
kevin-64 <kevinik@hotmail.it>
2023-09-15 20:52:19 -0700

    Create react/webpack application skeleton

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>This is my application</div>
+  )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const { resolve } = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+
+module.exports = () => {
+  return {
+    mode: 'development',
+    entry: "./src/index.tsx",
+    resolve: {
+      extensions: ['.js', '.jsx', '.ts', '.tsx'],
+    },
+    context: resolve(__dirname, './'),
+    module: {
+      rules: [
+        // {
+        //     test: /\.jpe?g|png$/,
+        //     exclude: /node_modules/,
+        //     use: ["url-loader", "file-loader"]
+        // },
+        {
+            test: /\.(ts|tsx)$/,
+            exclude: /node_modules/,
+            loader: "ts-loader"
+        }
+      ]
+  },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./public/index.html"
+        }),
+    ]
+  }
+};
