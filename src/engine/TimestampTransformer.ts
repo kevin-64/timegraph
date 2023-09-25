92aab0f52bac449a974241f66dac1bed6448ea40
kevin-64 <kevinik@hotmail.it>
2023-09-24 23:28:19 -0700

    Add timestamp manip functions, tests

--- src/engine/TimestampTransformer.ts
+++ src/engine/TimestampTransformer.ts
@@ -1,8 +1,45 @@
 import Timestamp from "../types/Timestamp";
 
-const transform = (timestamp: Timestamp) => {
-  const secValue = (timestamp.hh * 3600) + ((timestamp.mm || 0) * 60) + (timestamp.ss || 0)
-  return secValue/86400
+const SECONDS_IN_DAY = 86400
+const SECONDS_IN_HOUR = 3600
+const SECONDS_IN_MINUTE = 60
+
+const toTimestamp = (secs: number): Timestamp => {
+  let remaining = secs;
+
+  const hh = Math.floor(remaining / SECONDS_IN_HOUR)
+  remaining -= (hh*SECONDS_IN_HOUR)
+
+  const mm = Math.floor(remaining / SECONDS_IN_MINUTE)
+  remaining -= (mm*SECONDS_IN_MINUTE)
+
+  const ss = remaining
+
+  //TODO: handle days
+  return { hh, mm, ss }
 }
 
-export default transform;
+const toSeconds = (time: Timestamp): number => {
+  return (time.hh * SECONDS_IN_HOUR) + ((time.mm || 0) * SECONDS_IN_MINUTE) + (time.ss || 0)
+}
+
+const transform = (timestamp: Timestamp) => {
+  const secValue = (timestamp.hh * SECONDS_IN_HOUR) + ((timestamp.mm || 0) * SECONDS_IN_MINUTE) + (timestamp.ss || 0)
+  return secValue/SECONDS_IN_DAY
+}
+
+const sum = (lhs: Timestamp, rhs: Timestamp): Timestamp => {
+  const left = toSeconds(lhs)
+  const right = toSeconds(rhs)
+
+  return toTimestamp(left + right)
+}
+
+const diff = (lhs: Timestamp, rhs: Timestamp): Timestamp => {
+  const left = toSeconds(lhs)
+  const right = toSeconds(rhs)
+  return toTimestamp(left - right)
+}
+
+export default transform
+export { sum, diff }
---
+++ test/TimestampTransformer.test.ts
@@ -0,0 +1,13 @@
+import { sum, diff } from "../src/engine/TimestampTransformer"
+
+describe('TimestampTransformer', () => {
+  test('should add two times together', () => {
+    let left = { hh: 5, mm: 30 }
+    let right = { hh: 2, mm: 15 }
+    expect(sum(left, right)).toMatchObject({hh: 7, mm: 45})
+
+    left = { hh: 5, mm: 30 }
+    right = { hh: 3, mm: 45 }
+    expect(sum(left, right)).toMatchObject({hh: 9, mm: 15})
+  })
+})
